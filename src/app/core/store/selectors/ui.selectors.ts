5c28a47c45e7b2cf44185bbebe5d8820e8ad8cf6
Edward Łuska <edi@nabi.pl>
2022-03-03 22:34:29 +0100

    Let's say it's working

--- src/app/core/store/selectors/ui.selectors.ts
+++ src/app/core/store/selectors/ui.selectors.ts
@@ -2,3 +2,5 @@ import { UiState } from "src/app/interfaces/ui-state.interface";
 
 export const getUi = (state: UiState) => state.ui;
 export const getItemsPerPage = (state: UiState) => state.ui.itemsPerPage;
+export const getCategories = (state: UiState) => state.ui.categories;
+export const getOrder = (state: UiState) => state.ui.order;
